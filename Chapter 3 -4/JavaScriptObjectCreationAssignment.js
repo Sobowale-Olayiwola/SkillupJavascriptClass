c6c0dc19c3224d0d0209367817b664f14731fc5d
Sobowale-Olayiwola <layitheinfotechguru@gmail.com>
2021-03-16 09:55:35 +0100

    Assignment involving creation of Objects and Explaining DOM

---
+++ Chapter 3 -4/JavaScriptObjectCreationAssignment.js
@@ -0,0 +1,84 @@
+/*******Create 3 realistic objects with at least 10 key-value pairs
+One of the 3 objects should contain a key with another object value which 
+in turn has 5 key-value pairs************/
+
+//First Object
+let sport = {
+    name: 'BasketBall' ,
+    nameOfFounder: 'James Naismith' ,
+    teamMembersPerSide: 5 ,
+    equipment:'basketball' ,
+    positionsOfPlayer:['Center', 'Point guard', 'Shooting guard', 'Power forward', 'Small forward'] ,
+    rimHeight:'10 feet' ,
+    courtDiameter: '9.51 inches' ,
+    highestGoverningBody: 'FIBA' ,
+    playedInOlympics: true ,
+    countrySpecific: false ,
+    indoorAndOutdoorSport: true ,
+    playedInParalympic: true
+};
+
+//Second Object
+
+let song = {
+    name: 'Way Too big',
+    artist: 'Burna Boy',
+    album: 'Twice As Tall',
+    artistHomeCountry:'Nigeria',
+    minutes: 3 ,
+    seconds: 20 ,
+    genre: 'Afro Pop' ,
+    playing: false ,
+    wonAward: true ,
+    size: '6.59MB' ,
+    bitrate: '256 KB/s'
+};
+
+//
+
+//******Create 3 realistic arrays with at least 3 other realistic arrays as elements******
+
+//First Array
+
+let car = [['black','white','grey','blue','red'],['Mercedes Benz','Ford','Toyota','Volkswagen','Nissan'],[2010,2011,2012,2013,2014]];
+
+//Second Array
+
+let primarySchoolClass = [['Thomas','Martins','John','Milner'],['Partey','Obafemi','Stones','James'],['Primary 1','Primary 2', 'Primary 3', 'Primary 4']];
+
+//Third Array
+
+let church = [['Drum Set','Guitar','SaxoPhone','Piano','Talking Drum','Violin'],['Pastor','Chorister','Usher','Youth','Adult','Children'],[5,20,15,100,45,60]];
+
+//*******Create an array with two realistic objects as its elements********
+
+//creation of the first object
+let cadi = {
+    make: "GM",
+    model: "Cadillac",
+    year: 1955,
+    color: "tan",
+    passengers: 5,
+    convertible: false,
+    mileage: 12892
+    };
+
+//creation of the second object
+
+    let taxi = {
+        make: "Innoson Motors",
+        model: "Taxi",
+        year: 1955,
+        color: "yellow",
+        passengers: 4,
+        convertible: false,
+        mileage: 281341
+        };
+
+//array consisting of car objects
+
+let cars = [taxi,cadi];
+
+    
+
+        
---
+++ markdownfiles/explanationOfJavaScriptDOM.md
@@ -0,0 +1,27 @@
+# Explanation of JavaScript DOM
+
+JavaScript and HTML are two different things. HTML is markup and JavaScript is the code written.
+
+They both interact through through a representation of a page, called the document object model, or 
+
+the DOM for short. DOM is created when the browser loads your page.
+
+With the object model, JavaScript gets all the power it needs to create  dynamic HTML:
+
+- JavaScript can change all the HTML elements in the page
+
+- JavaScript can change all the HTML attributes in the page
+
+- JavaScript can change all the CSS styles in the page
+
+- JavaScript can remove existing HTML elements and attributes
+
+- JavaScript can add new HTML elements and attributes
+
+- JavaScript can react to all existing HTML events in the page
+
+- JavaScript can create new HTML events in the page
+
+An example of this can be seen when a browser loads this HTML file.
+
+![DOM tree of an example web page](https://res.cloudinary.com/layitheinfotechguru/image/upload/v1615884709/Dom-Tree-of-An-Example-Web-Page_mihw9x.png)
